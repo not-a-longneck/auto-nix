513fe78517ae2edc6741e26b75e3998291429c10
not-a-longneck <118153658+not-a-longneck@users.noreply.github.com>
2026-03-31 17:47:27 +0200

    Specify device variable in NixOS configuration
    Added specialArgs to specify the device variable.

--- flake.nix
+++ flake.nix
@@ -8,6 +8,11 @@
   outputs = { self, nixpkgs, disko, ... }: {
     nixosConfigurations.secure-laptop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      
+      # THIS IS THE MISSING PIECE:
+      # It tells Nix to expect a 'device' variable (defaulting to /dev/vda)
+      specialArgs = { device = "/dev/vda"; }; 
+
       modules = [
         disko.nixosModules.disko
         ./disko-config.nix
