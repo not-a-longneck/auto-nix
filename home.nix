5de195cbf9f87715f32fdd899cb83d99aced43d2
not-a-longneck <118153658+not-a-longneck@users.noreply.github.com>
2026-03-31 22:26:28 +0200

    Update Tor browser preferences in home.nix
    Added privacy and security preferences for Tor browser.

--- home.nix
+++ home.nix
@@ -36,7 +36,24 @@
 
   # Tor browser
   home.file.".tor-browser/profile.default/prefs.js".text = ''
+
+    // --- Privacy & Security ---
     user_pref("javascript.enabled", false);
+    user_pref("intl.accept_languages", "en-US, en");
+    user_pref("intl.locale.requested", "en-US");
+
+    // --- UI & Behavior ---
+    // Hide Bookmark Bar (2 = never)
+    user_pref("browser.toolbars.bookmarks.visibility", "never");
+    
+    // Always connect automatically
+    user_pref("extensions.torlauncher.prompt_at_startup", false);
+    user_pref("network.bootstrapped", true);
+
+    // --- Extra Hardening ---
+    user_pref("privacy.resistFingerprinting", true);
+    user_pref("webgl.disabled", true);
+    
   '';
 
 
