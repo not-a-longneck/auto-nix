da7e8d95d2be9bd86404a244497f1f632d0460cf
not-a-longneck <118153658+not-a-longneck@users.noreply.github.com>
2026-03-31 22:54:16 +0200

    Add Disko input and module to flake.nix

--- flake.nix
+++ flake.nix
@@ -1,24 +1,31 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # 1. Add the Disko input
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: {
+  outputs = { self, nixpkgs, disko, home-manager, ... }: {
     nixosConfigurations.secure-laptop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
+        # 2. Add the Disko module here
+        disko.nixosModules.disko
         ./configuration.nix
+        # 3. Reference your disk config file
         ./disko-config.nix
-        # Add this line to link HM to your NixOS config
+        
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          home-manager.users.admin = import ./home.nix; # 'admin' is your username
+          home-manager.users.admin = import ./home.nix;
         }
       ];
     };
