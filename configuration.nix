44848c4c1bf9578194bdbb60d8c17e644cf06a33
not-a-longneck <118153658+not-a-longneck@users.noreply.github.com>
2026-03-31 12:17:16 +0200

    Update configuration.nix for hardware and user setup

--- configuration.nix
+++ configuration.nix
@@ -1,20 +1,63 @@
 { pkgs, ... }: {
-  # Universal Hardware Support
+  
+  =============================
+  ==== Hardware & Firmware ====
+  =============================
+
   boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" "nvme" "usb_storage" ];
   hardware.enableAllFirmware = true;
+  nixpkgs.config.allowUnfree = true;
 
-  # Auto-rebuild on boot: Checks GitHub for changes
+  # Storage Optimization (The "Tails" Buffer)
+  # This allows /tmp to use up to 90% of available RAM + Swap 
+  # Perfect for those 20GB+ downloads.
+  boot.tmp.useTmpfs = true;
+  boot.tmp.tmpfsSize = "90%"; 
+
+  # Auto-rebuild on boot
   system.autoUpgrade = {
     enable = true;
-    flake = "github:your-user/my-secure-nixos";
+    flake = "github:not-a-longneck/auto-nix";
     flags = [ "--update-input" "nixpkgs" ];
     dates = "reboot"; 
   };
 
-  # Basic Setup
+  =============================
+  ==== Apps and tools      ====
+  =============================
+
+  environment.systemPackages = with pkgs; [
+    tor-browser
+    firefox
+    vlc
+    veracrypt
+    pyload-ng
+    sunshine
+    
+    
+  ];
+  
+  services.tor.enable = true;
+  services.tor.client.enable = true;
+
+  # 5. Desktop Environment (GNOME)
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true; # Change to KDE if you prefer!
+  services.xserver.desktopManager.gnome.enable = true;
 
+  # 6. User Setup
+
+# Define your personal user
+  users.users.admin = {
+    isNormalUser = true;
+    description = "Privacy User";
+    extraGroups = [ "networkmanager" "wheel" "video" ];
+    # This password will be active every time you boot
+    hashedPassword = "$6$Osqk1/PTMVPFxz.R$xnhXNz5ePRgPQZtGMaXlSDInDsrwNocuRqVmTfZcq4ujAer6PiesG27vZpkxdMJh3gtSzP9qOlTs8CTP9Pf.f/"; 
+  };
+
+  # Disable Guest/Auto-login for Security
+  services.displayManager.autoLogin.enable = false;
+  services.xserver.displayManager.gdm.wayland = true; # Modern & Secure
   system.stateVersion = "23.11";
 }
