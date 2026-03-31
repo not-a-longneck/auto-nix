88c3bae6ad6f3bf91985dad916388302ce1e8ff8
not-a-longneck <118153658+not-a-longneck@users.noreply.github.com>
2026-03-31 17:14:50 +0200

    Remove device variable from NixOS configuration
    Removed the specialArgs for device variable.

--- flake.nix
+++ flake.nix
@@ -8,8 +8,6 @@
   outputs = { self, nixpkgs, disko, ... }: {
     nixosConfigurations.secure-laptop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      # This passes the 'device' variable to all modules
-      specialArgs = { device = "/dev/vda"; }; 
       modules = [
         disko.nixosModules.disko
         ./disko-config.nix
